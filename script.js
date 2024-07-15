c60f1bf23883d40a6fff3268e5b1f59111913c99
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-07-15 16:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+
+let promise = new Promise(function(resolve, reject) {
+
+	setTimeout(() => resolve(output.innerHTML = "Hello, world!"), 1000)
+});
